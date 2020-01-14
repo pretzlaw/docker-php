ceccfea564807d8fa71c40ae789cf6ff859380b9
rmp-up <mail+99@rmp-up.de>
2020-01-14 03:09:45 +0100

    doc: Add some examples
    When cloning the repository you may like to test things.
    For this we brought up an example page showing a success message
    and phpinfo.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+<body>
+
+<h1>Hello there!</h1>
+
+<p>
+    Congratulations!
+    You successfully started the container.
+    Add some "docker-compose.yml" (e.g. the one from the README.md) in your project
+    to make use of the different PHP versions.
+</p>
+
+<hr>
+
+<?php phpinfo(); ?>
+
+</body>
+</html>
